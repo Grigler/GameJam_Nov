9c82631911f72461633508823433a31d8144535b
Kristiyan Stanev (i7620756) <i7620756@bournemouth.ac.uk>
2017-11-07 19:13:59 +0000

    Portal Test Scene

---
+++ GameJam_Nov_Unity/Assets/PortalLookCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalLookCheck : MonoBehaviour {
+
+    public GameObject camruh;
+    public GameObject destination;
+    public float stareTime;
+	// Use this for initialization
+	void Start () {
+        stareTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(camruh.transform.position, camruh.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 2))
+        {
+            if (hit.collider.gameObject.tag == "Portal")
+            {
+                stareTime += Time.deltaTime;
+            }
+            else
+            {
+                stareTime = 0;
+            }
+        }
+
+        if(stareTime > 3)
+        {
+            transform.position = destination.transform.position;
+        }
+
+	}
+}

018160a4bea512f558c11c56ccd898d6f3875e8a
Kristiyan Stanev (i7620756) <i7620756@bournemouth.ac.uk>
2017-11-07 11:51:08 +0000

    Pyramid dab

---
+++ GameJam_Nov_Unity/Assets/Pyramid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pyramid : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddTorque(transform.up * 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GameJam_Nov_Unity/Assets/bob.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bob : MonoBehaviour {
+
+    public float Greg;
+
+	// Use this for initialization
+	void Start () {
+        Greg = 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {       
+
+        transform.Translate(0, Mathf.Cos(Time.time) * Greg, 0);
+        
+	}
+}

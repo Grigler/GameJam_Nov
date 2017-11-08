8a463cabab6de36b46f55b291738e679d326e899
Jakub Szlachcic (s4912115) <s4912115@bournemouth.ac.uk>
2017-11-08 12:38:00 +0000

    final push before test

---
+++ GameJam_Nov_Unity/Assets/MaterialColour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColour : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        
+        Material mat = gameObject.GetComponent<MeshRenderer>().material;
+        mat.SetColor("Albedo", new Color(Random.Range(0, 1000.0f) / 1000.0f, Random.Range(0, 1000.0f) / 1000.0f, Random.Range(0, 1000.0f) / 1000.0f));
+
+        
+	}
+	
+}

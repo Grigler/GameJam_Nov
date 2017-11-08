92285f70923e37cd386b2246cd7ede11afbde521
Jakub Szlachcic (s4912115) <s4912115@bournemouth.ac.uk>
2017-11-08 11:57:34 +0000

    Model Movement

---
+++ GameJam_Nov_Unity/Assets/MummyMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MummyMovement : MonoBehaviour {
+    void Update()
+    {
+        transform.Rotate(new Vector3(25.0f * Time.deltaTime, -25.0f * Time.deltaTime, 0.0f));
+
+    }
+}
+
+
